e881b7f8626bd495a2b39043d7558084cc4618da
andrewshek <shrek_ak@hotmail.com>
2021-03-28 21:07:34 +1100

    update test

--- src/test/java/com/store/groceryhelper/service/CategoryServiceTest.java
+++ src/test/java/com/store/groceryhelper/service/CategoryServiceTest.java
@@ -11,7 +11,7 @@ public class CategoryServiceTest {
     @Test
     public void shouldGetCategories() {
         var categories = new CategoryService().getAllCategories();
-        assertThat(categories.get(0)).isEqualTo(Category.DARIY);
+        assertThat(categories.get(0)).isEqualTo(Category.DAIRY);
         assertThat(categories.get(1)).isEqualTo(Category.FRUIT);
         assertThat(categories.get(2)).isEqualTo(Category.VEGETABLE);
         assertThat(categories.get(3)).isEqualTo(Category.OTHER);
